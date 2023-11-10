58745a0c266ef0c9367fa17a8b8bbc2481d87b50
Santiago Sabogal Lozano <125775758+ssabogall@users.noreply.github.com>
2023-11-09 21:31:30 -0500

    Create Practica-2.hs
    Haskell

---
+++ Practica-2.hs
@@ -0,0 +1,18 @@
+Entrega
+
+La solución planteada por el equipo debe realizarse en un script haciendo uso
+del lenguaje de programación Haskell. La entrega de la solución se realiza
+tanto por el sitio destinado a esta actividad en Interactiva como por un
+repositorio en Github. La entrega de la solución por interactiva debe ir
+acompañada de un archivo de texto que contenga el nombre de los
+integrantes y la dirección del repositorio en Github.
+Para la sustentación de la solución desarrollada, el equipo debe elaborar un
+video en el que se debe detallar paso a paso el proceso que se llevó a cabo,
+deben aparecer en el video y participar todos los integrantes que hacen parte
+del equipo. El video elaborado debe ser subido a Internet y debe generarse un
+enlace para que se pueda acceder a este para la correspondiente revisión de
+la entrega realizada. Este enlace debe incluirse también tanto en el archivo de
+texto que acompaña a la solución entregada por Interactiva como en el
+archivo readme que se debe incluir en al repositorio en Github.
+
+
